36f5a1608568387fb9a8cab6963837a995ec9d07
Jachada <carmen.m.ru.pa@gmail.com>
2021-10-09 15:19:15 +0200

    Ejercicio 10 de la presentación UD3. Introducción a PHP - Parte 1

---
+++ ejercicio10.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 10</title>
+</head>
+<body>
+    <?php
+        $num = 5;
+        $factorial = 1;
+
+        for ($i=1; $i <= $num; $i++) { 
+            $factorial *= $i;
+        }
+        echo "El factorial de $num es $factorial";
+        
+    ?>
+</body>
+</html>
